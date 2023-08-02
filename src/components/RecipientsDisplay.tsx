2150064b98972fb5438beceafdc62b2c0ca841c2
Rabab Shayra <rababshayradhruba@google.com>
2023-08-02 18:45:23 +0900

    update with logic

--- src/components/RecipientsDisplay.tsx
+++ src/components/RecipientsDisplay.tsx
@@ -1,19 +1,85 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 interface RecipientsProps{
     recipients:string[];
 }
-const MAX_RECIPIENT_WIDTH = 40;
 
 export default function RecipientsDisplay({recipients}:RecipientsProps) {
-    const CellContainer = styled.div`
+  
+  const textAreaRef = useRef(null);
+  const [badgeCount, setBadgeCount] = useState(recipients.length-1);
+  const [tempReciepts, setTempReciepts] = useState(recipients);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (textAreaRef.current) {
+        const cellWidth = textAreaRef.current.clientWidth ;
+        const textToMeasure = recipients.toString();
+        
+        const textWidth = getStringWidthInPixels(textToMeasure);
+        const trimmeArr = getTrimmedArr(recipients.join(", "), cellWidth);
+
+        setTempReciepts(trimmeArr);
+        setBadgeCount(recipients.length - trimmeArr.length);
+      }
+    };
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  function getTrimmedArr(text : string, maxWidth : number) : string[] {
+    let widthInPixels = getStringWidthInPixels(text+'...');
+    let trimmedText = text;
+    if (widthInPixels <= maxWidth) {
+      trimmedText = text;
+    } else {
+      while (widthInPixels > maxWidth && trimmedText.length > 0) {
+        trimmedText = trimmedText.slice(0, -1);
+        widthInPixels = getStringWidthInPixels(trimmedText);
+      }
+    }
+    console.log(trimmedText);
+    let tmpArr = trimmedText.split(", ");
+    //console.log(tmpArr);
+    const lstEmail = tmpArr[tmpArr.length - 1]
+    //console.log(lstEmail);
+    
+    if(recipients.indexOf(lstEmail)==-1){
+      tmpArr.pop();
+    }
+
+    // if(lstEmail.indexOf('.com')==-1){
+    //   tmpArr.pop();
+    // }
+   
+    if(tmpArr.length==0){
+      tmpArr.push(recipients[0]);
+    }
+    return tmpArr;
+  }
+
+  function getStringWidthInPixels(text: string): number {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    if (!context) {
+      throw new Error("Canvas context is not supported.");
+    }
+    const font = "16px Arial";
+    const metrics = context.measureText(text);
+    return metrics.width;
+  }
+
+  const CellContainer = styled.div`
     font-size: 16px;
     foreground-color: #333333;
-    padding-top: 5px;
-    padding-bottom: 5px;
-    padding-left: 10px;
-    padding-right: 10px;
+    // padding-top: 5px;
+    // padding-bottom: 5px;
+    // padding-left: 10px;
+    // padding-right: 10px;
     display: flex;
     `;  
     const TextArea = styled.div`
@@ -35,18 +101,13 @@ export default function RecipientsDisplay({recipients}:RecipientsProps) {
     margin-left: auto; 
     margin-right: 0;
     `;
-
-    const displayRecipients = ()=> {
-      const tempContainer = document.createElement('div')
-      tempContainer.style.display = 'inline-block'
-      tempContainer.innerText = recipients.toString()
-      return(tempContainer.innerText);
-   }
-   const badgeCount=recipients.length-1;
+  
       return (
         <CellContainer>
-        <TextArea>{recipients.toString()}</TextArea>    
-        {badgeCount>0&&(
+         <TextArea ref={textAreaRef}>
+          {badgeCount>0 ? (tempReciepts.join(", ") + ", ...") : tempReciepts.join(", ")}
+          </TextArea>    
+        {badgeCount>0 && (
             <Badge>+{badgeCount}</Badge>
         )}
         </CellContainer>
